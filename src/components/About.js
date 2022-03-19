ea5ed04a0a54bf6fd06257944bb21b05a8f49129
kslooi <jaylks96@gmail.com>
2022-03-20 00:45:25 +0800

    updated css styling of About page.

--- src/components/About.js
+++ src/components/About.js
@@ -6,20 +6,18 @@ import { faCogs, faCode, faTools } from '@fortawesome/free-solid-svg-icons'
 const skills = [{
   categoryName: 'Programming Language', 
   categoryIcon: <FontAwesomeIcon icon={faCode} style={{marginTop: '20px', color: 'white'}}/>,
-  list: ['C', 'C++', 'Python', 'Javascript']
+  list: ['C', 'C++', 'Python', 'JavaScript']
 }, {
   categoryName: 'Technical skill', 
   categoryIcon: <FontAwesomeIcon icon={faCogs} style={{marginTop: '20px', color: 'white'}}/>,
   list: ['Embedded programming', 'Image processing', 'Machine learning', 'Mobile app development', 'Web app development']
 }, {
-  categoryName: 'Software tool', 
+  categoryName: 'Software / Tool', 
   categoryIcon: <FontAwesomeIcon icon={faTools} style={{marginTop: '20px', color: 'white'}}/>,
-  list: ['MATLAB', 'Vector CANoe', 'Solidworks', 'Gazebo']
+  list: ['MATLAB', 'Oracle VirtualBox', 'Solidworks', 'Gazebo']
 }];
 
-const briefIntro = 'Hi, I\'m Looi Kian Seong. \n' +
-                    'I am a highly motivated Software Engineer with focus on Embedded System, Robotics and Automotive. \n' + 
-                    'I always get myself immersed in variety of interesting software projects. ';
+const briefIntro = 'A highly motivated Software Engineer with focus on Embedded System, Robotics and Automotive. ';
 
 class About extends React.Component {
    constructor (props) {
@@ -29,7 +27,7 @@ class About extends React.Component {
 
    renderSkillCategories = (category, categoryIndex) => {
        return (
-           <div key={categoryIndex} className='Skill-card'>
+           <div key={categoryIndex} className='About-skill-card'>
                <div>
                    {category.categoryIcon}
                    {category.categoryName}
@@ -46,14 +44,12 @@ class About extends React.Component {
            <div className='About-main'>
                <h1> About Me </h1>
                <div className='About-intro'>
-                 <p>
-                  {briefIntro}
-                 </p>
+                   <p> {briefIntro} </p>
                </div>
                
-               <div className='Skills-main'>
+               <div className='About-skills'>
                    <h2> Skills </h2>
-                   <div className='Skill-cards-container'>
+                   <div className='About-skill-cards-container'>
                        {skills.map(this.renderSkillCategories)}
                     </div>
                 </div>
