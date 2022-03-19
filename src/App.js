f03c214c9a7c139c765e085631fb95721df6d680
kslooi <jaylks96@gmail.com>
2022-03-19 20:27:34 +0800

    changed top-level component to class component

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Projects from './components/Projects.js';
 import Contact from './components/Contact.js';
 
 import './App.css';
+import React from 'react';
 
 const navlinks = [
   { name: 'About', to: '/about' }, 
@@ -23,62 +24,71 @@ const navlinks = [
   // { name: 'Contact', to: '#contact' }, 
 ];
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-          <a href='/' className='App-logo'> </a>
-          <Navbar navlinks={navlinks} contentTopOffset='70px'/>
-      </header>
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
       
-      <div className='App-content'>
-        <Routes>
-          <Route index path='/' element={<Home/>}/>
-          <Route path='About' element={<About/>}/>
-          <Route path='Background' element={<Background/>}/>
-          <Route path='Achievements' element={<Achievements/>}/>
-          <Route path='Blog' element={<Blog/>}/>
-          <Route path='Projects' element={<Projects/>}/>
-          <Route path='#Contact' element={<Contact/>}/>
-        </Routes>
-      </div>
+    };
+  }
 
-      <footer className='App-footer'>
-        <div id='contact'>
-          <h3>
-            Looi Kian Seong
-          </h3>
-          
-          <div>
-            <FontAwesomeIcon icon={faEnvelope} size='sm'/>
-            <a className='Email' href='mailto:kianseong.looi@gmail.com'> kianseong.looi@gmail.com </a>
-          </div>
-          
-          <ul>
-            <li>
-              <a href='https://linkedin.com/in/kianseong' rel='noreferrer' target='_blank'>
-                <FontAwesomeIcon icon={faLinkedin} size='2x'/>
-              </a>
-            </li>
-
-            <li>
-              <a href='https://github.com/JayLooi' rel='noreferrer' target='_blank'>
-                <FontAwesomeIcon icon={faGithub} size='2x'/>
-              </a>
-            </li>
-
-            <li>
-              <a href='https://medium.com/@kslooi' rel='noreferrer' target='_blank'>
-                <FontAwesomeIcon icon={faMedium} size='2x'/>
-              </a>
-            </li>
-          </ul>
-
-          <hr/>
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+            <a href='/' className='App-logo'> </a>
+            <Navbar navlinks={navlinks}/>
+        </header>
+        
+        <div className='App-content'>
+          <Routes>
+            <Route index path='/' element={<Home/>}/>
+            <Route path='About' element={<About/>}/>
+            <Route path='Background' element={<Background/>}/>
+            <Route path='Achievements' element={<Achievements/>}/>
+            <Route path='Blog' element={<Blog/>}/>
+            <Route path='Projects' element={<Projects/>}/>
+            <Route path='#Contact' element={<Contact/>}/>
+          </Routes>
         </div>
-      </footer>
-    </div>
-  );
+
+        <footer className='App-footer'>
+          <div id='contact'>
+            <h3>
+              Looi Kian Seong
+            </h3>
+            
+            <div>
+              <FontAwesomeIcon icon={faEnvelope} size='sm'/>
+              <a className='Email' href='mailto:kianseong.looi@gmail.com'> kianseong.looi@gmail.com </a>
+            </div>
+            
+            <ul>
+              <li>
+                <a href='https://linkedin.com/in/kianseong' rel='noreferrer' target='_blank'>
+                  <FontAwesomeIcon icon={faLinkedin} size='2x'/>
+                </a>
+              </li>
+
+              <li>
+                <a href='https://github.com/JayLooi' rel='noreferrer' target='_blank'>
+                  <FontAwesomeIcon icon={faGithub} size='2x'/>
+                </a>
+              </li>
+
+              <li>
+                <a href='https://medium.com/@kslooi' rel='noreferrer' target='_blank'>
+                  <FontAwesomeIcon icon={faMedium} size='2x'/>
+                </a>
+              </li>
+            </ul>
+
+            <hr/>
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
